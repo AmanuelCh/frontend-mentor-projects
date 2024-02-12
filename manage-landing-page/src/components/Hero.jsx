da91b3317555b3edd70bf3939035498b807c6c81
AmanuelCh <amanuelchaka2@gmail.com>
2024-02-12 23:08:41 +0300

    hero section completed

--- manage-landing-page/src/components/Hero.jsx
+++ manage-landing-page/src/components/Hero.jsx
@@ -1,7 +1,22 @@
+import heroImage from "../images/illustration-intro.svg";
+
 const Hero = () => {
   return (
-    <div>
-      <p className="text-base">Hero</p>
+    <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
+      <div className="flex flex-col mb-32 space-y-6 md:w-1/2">
+        <h3 className="max-w-md text-4xl font-bold text-center text-darkBlue mt-6 md:text-5xl md:text-left md:mt-0">Bring everyone together to build better products</h3>
+        <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+          Manage makes it simple for software teams to plan day-to-day tasks
+          while keeping the larger team goals in view.
+          </p>
+          <div className="flex items-center justify-center md:block pt-3">
+          <a href="#" className="bg-brightRed p-3 px-6 rounded-full text-white transition-opacity hover:opacity-70">Get started</a>
+          </div>
+        
+      </div>
+      <div className="md:w-1/2">
+        <img src={heroImage} alt="Hero Image" />
+      </div>
     </div>
   );
 };
--- manage-landing-page/src/components/Navbar.jsx
+++ manage-landing-page/src/components/Navbar.jsx
@@ -1,52 +1,80 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import logo from '../images/logo.svg'
-import openMenu from '../images/icon-hamburger.svg'
-import closeMenu from '../images/icon-close.svg'
-import background from '../images/bg-tablet-pattern.svg'
+import logo from "../images/logo.svg";
+import openMenu from "../images/icon-hamburger.svg";
+import closeMenu from "../images/icon-close.svg";
+import background from "../images/bg-tablet-pattern.svg";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
-    !isOpen ? setIsOpen(true) : setIsOpen(false)
-  }
+    !isOpen ? setIsOpen(true) : setIsOpen(false);
+  };
 
   return (
     <>
-    <div className="container mx-auto mt-10 px-6 flex justify-between items-center relative lg:py-6">
-      <div>
-        <img src={logo} className='cursor-pointer' alt="manage logo" />
-      </div>
-      <div className='hidden space-x-6 text-veryDarkBlue md:text-sm md:flex'>
-        <a href="#" className='hover:text-darkGrayishBlue transition-colors'>Pricing</a>
-        <a href="#" className='hover:text-darkGrayishBlue transition-colors'>Product</a>
-        <a href="#" className='hover:text-darkGrayishBlue transition-colors'>About Us</a>
-        <a href="#" className='hover:text-darkGrayishBlue transition-colors'>Careers</a>
-        <a href="#" className='hover:text-darkGrayishBlue transition-colors'>Community</a>
-      </div>
-      <div className='hidden md:flex'>
-        <a href="#" className="bg-brightRed p-3 px-6 rounded-full text-white transition-opacity hover:opacity-70">Get Started</a>
-      </div>
+      <div className="container mx-auto mt-10 px-6 flex justify-between items-center relative lg:py-6">
+        <div>
+          <img src={logo} className="cursor-pointer" alt="manage logo" />
+        </div>
+        <div className="hidden space-x-6 text-veryDarkBlue md:text-sm md:flex">
+          <a href="#" className="hover:text-darkGrayishBlue transition-colors">
+            Pricing
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue transition-colors">
+            Product
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue transition-colors">
+            About Us
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue transition-colors">
+            Careers
+          </a>
+          <a href="#" className="hover:text-darkGrayishBlue transition-colors">
+            Community
+          </a>
+        </div>
+        <div className="hidden md:flex">
+          <a
+            href="#"
+            className="bg-brightRed p-3 px-6 rounded-full text-white transition-opacity hover:opacity-70"
+          >
+            Get Started
+          </a>
+        </div>
 
-      <div className='md:hidden'>
-        <img src={!isOpen ? openMenu : closeMenu} className='cursor-pointer' onClick={toggleMenu} alt="menu" />
+        <div className="md:hidden">
+          <img
+            src={!isOpen ? openMenu : closeMenu}
+            className="cursor-pointer"
+            onClick={toggleMenu}
+            alt="menu"
+          />
+        </div>
       </div>
-
-    </div>
-
-    <img src={background} alt="" className="hidden background absolute -top-24 -right-24 -z-10 max-w-xl lg:max-w-2xl  sm:block" />
-
-    <div className="md:hidden">
-      <div className={`absolute flex-col items-center ${isOpen ? 'flex' : 'hidden'} self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md text-veryDarkBlue`}>
-        <a href="#">Pricing</a>
-        <a href="#">Product</a>
-        <a href="#">About Us</a>
-        <a href="#">Careers</a>
-        <a href="#">Community</a>
+      <div className="background-container">
+        <div className="bg-pattern bg1"></div>
+        <div className="bg-pattern bg2"></div>
       </div>
-    </div>
-
+      {/* <img
+        src={background}
+        alt=""
+        className="hidden background absolute -top-1/4 -right-1/4 -z-10 sm:block lg:-top-16 lg:-right-16"
+      /> */}
+      <div className="md:hidden">
+        <div
+          className={`absolute flex-col items-center ${
+            isOpen ? "flex" : "hidden"
+          } self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md text-veryDarkBlue`}
+        >
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
+        </div>
+      </div>
     </>
   );
 };
