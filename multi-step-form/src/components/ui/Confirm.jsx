aa123b3c0fe0027c5a0d484dec281f7a6f74cb72
AmanuelCh <amanuelchaka2@gmail.com>
2024-05-08 15:37:43 +0300

    Confirm section done

--- multi-step-form/src/components/ui/Confirm.jsx
+++ multi-step-form/src/components/ui/Confirm.jsx
@@ -1,7 +1,19 @@
+import confirmSvg from '../../../public/assets/images/icon-thank-you.svg';
+
 const Confirm = () => {
   return (
     <div>
-      <p>Confirmed</p>
+      <div className='flex flex-col items-center gap-2 mt-8 lg:mt-20'>
+        <img src={confirmSvg} />
+        <h2 className='lg:text-4xl text-3xl text-marineBlue font-semibold font-ubuntuMedium mt-6'>
+          Thank You!
+        </h2>
+        <p className='mt-3 mb-8 text-coolGray text-center'>
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremgaming.com
+        </p>
+      </div>
     </div>
   );
 };
