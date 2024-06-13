31aa3b247000adf8f89dc8e7de776523ac6c3c39
Amanuel Chaka <amigojr2030@gmail.com>
2024-06-13 22:04:36 +0300

    init tailwind

---
+++ ecommerce-product-page/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'orange-primary': 'hsl(26, 100%, 55%)',
+        'pale-orange': 'hsl(25, 100%, 94%)',
+        'very-dark-blue': 'hsl(220, 13%, 13%)',
+        'dark-grayish-blue': 'hsl(219, 9%, 45%)',
+        'grayish-blue': 'hsl(220, 14%, 75%)',
+        'light-grayish-blue': 'hsl(223, 64%, 98%)',
+        white: 'hsl(0, 0%, 100%)',
+        // with 75% opacity for lightbox
+        black: 'hsl(0, 0%, 0%)',
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+  },
+  plugins: [],
+};
